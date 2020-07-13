b87c50db2187d67fcdb714bcf58c3787951c57a4
Caleb335 <belac335@gmail.com>
2020-07-13 22:26:41 +0100

    refactor form capture

--- index.php
+++ index.php
@@ -8,23 +8,24 @@
 <body>
     <!-- executes when the form is submitted -->
     <?php
-    // adding the validation script
-    if($_POST['submitForm'] === 'submit') {
-        $errorMessage = '';
-        
-        if(empty($_POST['fullname'])) {
-            $errorMessage .= '<li>You forgot to enter your fullname!</li>';
-        } if(empty($_POST['email'])) {
-            $errorMessage .= '<li>You forgot to enter your email address</li>';
-        }
+        if($_POST['submitForm'] === 'submit') {
+            $errorMessage = '';
+            
+            // adding the validation script
+            if(empty($_POST['fullname'])) {
+                $errorMessage .= '<li>You forgot to enter your fullname!</li>';
+            } if(empty($_POST['email'])) {
+                $errorMessage .= '<li>You forgot to enter your email address</li>';
+            }
 
-        $email = $_POST['email'];
-        $name = $_POST['fullname'];
+            // form variables
+            $email = $_POST['email'];
+            $name = $_POST['fullname'];
 
-        if(!empty($errorMessage)) {
-            echo('<p>There was an error with your form: </p>\n');
-            echo('<ul>' . $errorMessage . '</ul>\n');
-        } else {
+            if(!empty($errorMessage)) {
+                echo('<p>There was an error with your form: </p>\n');
+                echo('<ul>' . $errorMessage . '</ul>\n');
+            } else {
             // writes the form input(data) to a CSV file called 'mydata.csv'
             $fs = fopen("mydata.csv", "a"); 
                 // concatenates, join both strings or values recieved from the form submission
@@ -34,30 +35,18 @@
                 // redirects to a new html page on form submission
                 header("Location: ./thankyou.html"); 
                 exit;
+            }
         }
-    } 
-        if(isset($_POST['form_submitted'])): 
     ?>
-    <h2>Thank you <?php echo $_POST['firstname']; ?> </h2>
-    <?php 
-        else: 
-    ?>
-    <h2>Newsletter</h2>
-        <form action="./index.php" method="POST">
-            <label for="name">Full Name</label>
-            <div class="input-group">
-                <input type="text" name="fullname" placeholder="fullname" value="<?=$name;?>">
-            </div>
-            <label for="Email">Email</label>
-            <div class="input-group">
-                <input type="email" name="email" placeholder="Email" value="<?=$email;?>">
-            </div>
-            <div class="input-group">
-                <input type="submit" value="submit" name="submitForm">
-            </div>
-        </form>
-    <?php 
-        endif;
-    ?>
+
+    <form action="./index.php" method="POST">
+        <div class="input-group">
+            <input type="text" name="fullname" placeholder="full name" value="<?=$name;?>">
+        </div>
+        <div class="input-group">
+            <input type="email" name="fullname" placeholder="full name" value="<?=$email;?>">
+        </div>
+        <input type="submit" value="submit" name="submitForm">
+    </form>
 </body>
 </html>
