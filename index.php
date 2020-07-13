f1ce876743ad09738196b519bbe0b79c02540bc3
Caleb335 <belac335@gmail.com>
2020-07-13 15:29:47 +0100

    add validation

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
 </head>
 <body>
     <!-- executes when the form is submitted -->
-    <?php  
+    <?php
+    // adding the validation script
+    if($_POST['submitForm'] === 'submit') {
+        $errorMessage = '';
+        
+        if(empty($_POST['fullname'])) {
+            $errorMessage .= '<li>You forgot to enter your fullname!</li>';
+        } if(empty($_POST['email'])) {
+            $errorMessage .= '<li>You forgot to enter your email address</li>';
+        }
+
+        $email = $_POST['email'];
+        $name = $_POST['fullname'];
+
+        if(!empty($errorMessage)) {
+            echo('<p>There was an error with your form: </p>\n');
+            echo('<ul>' . $errorMessage . '</ul>\n');
+        }
+    } 
         if(isset($_POST['form_submitted'])): 
     ?>
     <h2>Thank you <?php echo $_POST['firstname']; ?> </h2>
@@ -15,13 +33,17 @@
         else: 
     ?>
     <h2>Newsletter</h2>
-        <form action="./app_release.php" method="POST">
+        <form action="./index.php" method="POST">
+            <label for="name">Full Name</label>
+            <div class="input-group">
+                <input type="text" name="fullname" placeholder="fullname" value="<?=$name;?>">
+            </div>
             <label for="Email">Email</label>
             <div class="input-group">
-                <input type="email" placeholder="Email" required>
+                <input type="email" name="email" placeholder="Email" value="<?=$email;?>">
             </div>
             <div class="input-group">
-                <input type="submit" value="submit">
+                <input type="submit" value="submit" name="submitForm">
             </div>
         </form>
     <?php 
