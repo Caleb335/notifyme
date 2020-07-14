75c6e269b8057d6c96f8b599fd293523151485e3
Caleb335 <belac335@gmail.com>
2020-07-14 13:00:18 +0100

    add landing

--- index.php
+++ index.php
@@ -3,25 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form subscription</title>
+    <title>Welcome &mdash; SpaceSee</title>
+    <!-- custom css -->
+    <link rel="stylesheet" href="./css/app.css">
+    <!-- favicon -->
+    <link rel="icon" href="./assets/see.png">
 </head>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    
-    .form-container {
-        display: flex;
-        justify-content: center;
-        margin-top: 12%;
-    }
-
-    .input-group {
-        margin-top: 20px;
-    }
-</style>
 <body>
     <!-- executes when the form is submitted -->
     <?php
@@ -29,9 +16,7 @@
             $errorMessage = '';
             
             // adding the validation script
-            if(empty($_POST['fullname'])) {
-                $errorMessage .= '<li>You forgot to enter your fullname!</li>';
-            } if(empty($_POST['email'])) {
+             if(empty($_POST['email'])) {
                 $errorMessage .= '<li>You forgot to enter your email address</li>';
             }
 
@@ -53,21 +38,94 @@
                 header("Location: ./thankyou.html"); 
                 exit;
             }
+
+             // create database connection and db variables
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "spaceSee";
+        
+            // connect to database
+            $conn = mysql_connect($servername, $username, $password, $dbname);
+            // if connection to the databse is not successful
+            // kill the process
+            if(!$conn) {
+                die("Connection failed: " . mysql_connect_error());
+            } else {
+                $sql = "INSERT INTO spaceSeeSubbers (email, fullname) values('$name', '$email')";
+                // if the database connnection has been created successfully print a message saying:
+                // new email address has been added
+                if(mysql_query($conn, $sql)) {
+                    echo 'New email address has been added successfully.';
+                } else {
+                    echo "Error: " .$sql . "</br>" . mysql_error($conn);
+                }
+            }
+
+            // close the connection to database
+            mysqli_close($conn);
         }
 
-        
     ?>
+    <div class="container">
+            <!-- head section -->
+    <section id="header">
+        <header>
+            <nav>
+                <div class="nav-brand">
+                    <img src="./assets/see.png" alt="">
+                    <div class="brand-text">
+                        <h3>spacesee</h3>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </section>
 
-    <div class="form-container">
-        <form action="./index.php" method="POST">
-            <div class="input-group">
-                <input type="text" name="fullname" placeholder="full name" value="<?=$name;?>">
+    <!-- main section -->
+    <section id="main">
+        <div class="hero">
+            <div class="hero-intro">
+                <h1>spacesee</h1>
+                <div class="hero-details">
+                    <p>Learn about your Universe and everything in it. The app that allows you to travel through space with out leaving where you are. </p>
+                </div>
+                <p class="cta">Coming soon...</p>
             </div>
-            <div class="input-group">
-                <input type="email" name="email" placeholder="email" value="<?=$email;?>">
+        </div>
+        <!-- know more -->
+        <div class="facts">
+            <div class="facts-intro">
+                <p>Did You Know?</p> <hr>
             </div>
-            <input type="submit" value="submit" name="submitForm">
-        </form>
+            <div class="fact-box one">
+                <p class="num">01</p>
+                <p>Neutron stars can spin at a rate of 600 rotations per second. </p>
+            </div>
+            <div class="fact-box two">
+                <p class="num">02</p>
+                <p>Neutron stars can spin at a rate of 600 rotations per second. </p>
+            </div>
+            <div class="fact-box three">
+                <p class="num">03</p>
+                <p>Neutron stars can spin at a rate of 600 rotations per second. </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- form section -->
+    <section id="form">
+        <div class="form-container">
+            <form action="./index.php" method="POST">
+                <div class="input-group">
+                    <input type="email" name="email" placeholder="Email Address" value="<?=$email;?>">
+                </div>
+                <div class="input-group">
+                    <input type="submit" value="Notify Me" name="submitForm">
+                </div>
+            </form>
+        </div>
+    </section>
     </div>
 </body>
 </html>
